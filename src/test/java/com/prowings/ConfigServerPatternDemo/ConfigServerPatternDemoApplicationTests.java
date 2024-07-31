cf12f0523a9ce3913c73d6cf04f9385bb40065cb
Khalipe <VJK1871997@gmail.com>
2024-07-31 15:18:02 +0530

    First Commit

---
+++ src/test/java/com/prowings/ConfigServerPatternDemo/ConfigServerPatternDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prowings.ConfigServerPatternDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerPatternDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
